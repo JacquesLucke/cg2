3943b9450b128c920cfcac0b20d968c71c250395
Jacques Lucke <mail@jlucke.com>
2018-04-23 22:28:10 +0200

    use imgui

--- source/main.cc
+++ source/main.cc
@@ -1,5 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw_gl3.h>
 #include "kdtree.h"
 #include "vector.h"
 #include <iostream>
@@ -27,12 +29,26 @@ int main(void)
 
     std::cout << "OpenGL: " << glGetString(GL_VERSION) << std::endl;
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui::StyleColorsDark();
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        ImGui::Text("Hello World");
+
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
