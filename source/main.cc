468d6efdf9b99ca46141a1b5b600e9325ca9ef14
Jacques Lucke <mail@jlucke.com>
2018-04-23 14:54:40 +0200

    restructure project and start to use cmake

--- source/main.cc
+++ source/main.cc
@@ -1,33 +1,3 @@
-/* install glfw:
-    https://github.com/glfw/glfw/issues/808
-
-    deb http://ppa.launchpad.net/keithw/glfw3/ubuntu trusty main
-
-    Then execute these commands
-
-    sudo apt-get update
-    sudo apt-get install libglfw3
-    sudo apt-get install libglfw3-dev
-
-    The library should now be available here:
-
-    /usr/lib/x86_64-linux-gnu/libglfw.so
-    /usr/lib/x86_64-linux-gnu/libglfw.so.3
-    /usr/lib/x86_64-linux-gnu/libglfw.so.3.1
-
-    And the headers here:
-
-    /usr/include/GLFW/glfw3.h
- */
-
-/* link against libglfw3 libGL
-    -lglfw3 -lGL
-*/
-
-/* Example show only empty window
-   and does not use 'glad' */
-
-
 #include <GLFW/glfw3.h>
 
 int main(void)
