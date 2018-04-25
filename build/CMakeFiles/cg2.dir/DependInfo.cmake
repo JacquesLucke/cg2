c66237ee949ff8fbc9d371feffa5eb3402e4822b
johannes <johannes@alcatraz.alcatraz.local>
2018-04-25 11:15:39 +0200

    Separated Source and Include files

---
+++ build/CMakeFiles/cg2.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/extern/glad/src/glad.c" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/extern/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../extern/glfw/include"
+  "../extern/glad/include"
+  "../extern/imgui"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/extern/imgui/imgui.cpp" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/extern/imgui/imgui.cpp.o"
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/extern/imgui/imgui_draw.cpp" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/extern/imgui/imgui_draw.cpp.o"
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/extern/imgui/imgui_impl_glfw_gl3.cpp" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/extern/imgui/imgui_impl_glfw_gl3.cpp.o"
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/source/main.cc" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/source/main.cc.o"
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/source/timer.cc" "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/CMakeFiles/cg2.dir/source/timer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../extern/glfw/include"
+  "../extern/glad/include"
+  "../extern/imgui"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/johannes/Documents/TU_Berlin/Informatik/Sem6/cg2/build/extern/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cg2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cg2.dir/source/main.cc.o"
+  "CMakeFiles/cg2.dir/source/timer.cc.o"
+  "CMakeFiles/cg2.dir/extern/glad/src/glad.c.o"
+  "CMakeFiles/cg2.dir/extern/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/cg2.dir/extern/imgui/imgui.cpp.o"
+  "CMakeFiles/cg2.dir/extern/imgui/imgui_impl_glfw_gl3.cpp.o"
+  "cg2.pdb"
+  "cg2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/cg2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
