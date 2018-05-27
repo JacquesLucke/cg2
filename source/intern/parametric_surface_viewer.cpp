8ad892624d9f727bef1209d0899b7ee2af64bcb8
Jacques Lucke <mail@jlucke.com>
2018-05-27 11:19:35 +0200

    draw Grid lines

--- source/intern/parametric_surface_viewer.cpp
+++ source/intern/parametric_surface_viewer.cpp
@@ -30,6 +30,25 @@ std::vector<glm::vec3> *calcGridPoints(int xDiv, int zDiv) {
     return points;
 }
 
+std::vector<EdgeIndices> *calcGridEdges(int div1, int div2) {
+    auto edges = new std::vector<EdgeIndices>();
+    for (int i = 0; i < div1; i++) {
+        for (int j = 0; j < div2 - 1; j++) {
+            edges->push_back(EdgeIndices(
+                i * div2 + j + 0,
+                i * div2 + j + 1));
+        }
+    }
+    for (int i = 0; i < div2; i++) {
+        for (int j = 0; j < div1 - 1; j++) {
+            edges->push_back(EdgeIndices(
+                (j + 0) * div2 + i,
+                (j + 1) * div2 + i));
+        }
+    }
+    return edges;
+}
+
 void ParametricSurfaceViewer::onRender() {
     int width, height;
     glfwGetFramebufferSize(window()->handle(), &width, &height);
@@ -45,7 +64,8 @@ void ParametricSurfaceViewer::onRender() {
     }
 
     auto vertices = createVertexPVector(*gridPoints);
-    PointCloud<VertexP> points(vertices);
+    auto edges = calcGridEdges(xDivisions, zDivisions);
+    Wireframe<VertexP> points(vertices, *edges);
 
     gridShader->bind();
     glPointSize(3);
