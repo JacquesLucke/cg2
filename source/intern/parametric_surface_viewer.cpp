166722aba9a3ab3481ad221ea09d816670de986f
Jacques Lucke <mail@jlucke.com>
2018-05-27 10:42:58 +0200

    draw grid points

--- source/intern/parametric_surface_viewer.cpp
+++ source/intern/parametric_surface_viewer.cpp
@@ -3,9 +3,55 @@
 #include <imgui_impl_glfw_gl3.h>
 
 #include "../parametric_surface_viewer.hpp"
+#include "../mesh_utils.hpp"
 
 bool ParametricSurfaceViewer::onSetup() {
+    gridShader = new FlatShader();
+
     return true;
+}
+
+std::vector<glm::vec3> *calcGridPoints(int xDiv, int zDiv) {
+    auto points = new std::vector<glm::vec3>();
+
+    float xOffset = -(xDiv - 1) / 2.0f;
+    float zOffset = -(zDiv - 1) / 2.0f;
+
+    for (float z = 0; z < zDiv; z++) {
+        for (float x = 0; x < xDiv; x++) {
+            points->push_back(glm::vec3(
+                x + xOffset,
+                0,
+                z + zOffset
+            ));
+        }
+    }
+
+    return points;
+}
+
+void ParametricSurfaceViewer::onRender() {
+    int width, height;
+    glfwGetFramebufferSize(window()->handle(), &width, &height);
+    glViewport(0, 0, width, height);
+    ((PerspectiveCamera*)camera->camera)->aspect = window()->aspect();
+
+    glm::mat4 matViewProj = camera->camera->getViewProjectionMatrix();
+    gridShader->bind();
+    gridShader->setTransforms(matViewProj);
+
+    if (gridPoints == nullptr) {
+        gridPoints = calcGridPoints(xDivisions, zDivisions);
+    }
+
+    auto vertices = createVertexPVector(*gridPoints);
+    PointCloud<VertexP> points(vertices);
+
+    gridShader->bind();
+    glPointSize(3);
+    gridShader->setColor(1, 1, 1);
+    points.bindBuffers(gridShader);
+    points.draw();
 }
 
 void ParametricSurfaceViewer::onRenderUI() {
--- source/parametric_surface_viewer.hpp
+++ source/parametric_surface_viewer.hpp
@@ -1,13 +1,31 @@
 #pragma once
 
+#include <glm/glm.hpp>
+#include <vector>
+
 #include "window_controller.hpp"
+#include "camera.hpp"
+#include "mesh.hpp"
+#include "shader.hpp"
 
 class ParametricSurfaceViewer : public WindowController {
 public:
     ParametricSurfaceViewer(Window* window)
-        : WindowController(window) {}
+        : WindowController(window),
+          camera(new CameraController(new PerspectiveCamera(
+              glm::vec3(5, 5, 5), glm::vec3(0, 0, 0), 1, window->aspect()), window)) {}
 
 protected:
     bool onSetup() final override;
+    void onRender() final override;
     void onRenderUI() final override;
+
+private:
+    CameraController* camera;
+
+    int xDivisions = 10;
+    int zDivisions = 10;
+
+    std::vector<glm::vec3>* gridPoints = nullptr;
+    FlatShader* gridShader = nullptr;
 };
