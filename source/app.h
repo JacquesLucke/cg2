2b8beae57d0d6d20749c490a4ee6e92af33017f8
ts <torstenschenk22@yahoo.de>
2018-05-29 23:52:47 +0200

    bug fix: black screen due to missing VertexArray
        needed, bug fix:
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
    
        is ok too:
         /*new*/ glBindBuffer(GL_ARRAY_BUFFER, 0);
        also:
         /*new*/ glBindVertexArray(0);
        but then again in:
        glBindVertexArray(VAO);
    
        further in teardown:
        glDeleteVertexArrays(1, &VAO);
        glDeleteBuffers(1, &VBO);

--- source/app.h
+++ source/app.h
@@ -4,7 +4,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-float positions[6] = {
+float vertices[6] = {
     -0.5f, -0.5f,
      0.0f,  0.5f,
      0.5f, -0.5f
@@ -17,6 +17,8 @@ public:
     App(GLFWwindow* window) {
         this->window = window;
     }
+
+    virtual ~App() {};
 
     virtual void setup() {}
     virtual void teardown() {}
@@ -68,19 +70,32 @@ class TestApp : public App {
 public:
     using App::App;
 
-    unsigned int buffer;
+    unsigned int VBO, VAO;
 
     void setup() {
         ImGui::CreateContext();
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
+/*new*/ glGenVertexArrays(1, &VAO);
+    	glGenBuffers(1, &VBO);
+    	// bind the Vertex Array Object first,
+    	// then bind and set vertex buffer(s),
+    	// and then configure vertex attributes(s).
+/*new*/ glBindVertexArray(VAO);
 
-        glGenBuffers(1, &buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);
+        //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
+
+ /*new*/ // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+ /*new*/ glBindBuffer(GL_ARRAY_BUFFER, 0);
+ /*new*/
+ /*new*/ // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+ /*new*/ // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+ /*new*/ glBindVertexArray(0);
 
         std::string vertexShader =
             "#version 330 core\n"
@@ -97,22 +112,30 @@ public:
             "out vec4 color;\n"
             "\n"
             "void main() {\n"
-            "   color = vec4(1.0, 1.0, 0.0, 1.0);\n"
-            "}\n";
+            "   color = vec4(0.5, 0.0, 0.5, 1.0);\n"
+            "}\n\0";
+
         unsigned int shader = createShader(vertexShader, fragmentShader);
         glUseProgram(shader);
     }
 
     void teardown() {
+        // optional: de-allocate all resources once they've outlived their purpose:
+        // ------------------------------------------------------------------------
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
         ImGui_ImplGlfwGL3_Shutdown();
         ImGui::DestroyContext();
     }
 
     void update() {
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        // ImGui_ImplGlfwGL3_NewFrame();
-        // ImGui::Text("Test App");
-        // ImGui::Render();
-        // ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+        ImGui_ImplGlfwGL3_NewFrame();
+        ImGui::Text("Test App");
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+        // glBindVertexArray(0); // no need to unbind it every time
+
     }
 };
